a836a400dd84eed2fdaac1a7c3d241894a81e090
flores2006 <andresjf260@gmail.com>
2024-11-19 12:07:54 -0400

    agregandosumaresta

---
+++ JS/main.js
@@ -0,0 +1,24 @@
+let mensaje= "Hola Mundo";
+
+
+let resta=numero1-numero2
+
+let multiplicacion=numero1*numero2
+
+let division=numero1/numero2
+
+
+console.log(mensaje);
+console.log("la suma es:",suma);
+console.log("la resta es:",resta);
+console.log("la multiplicacion es:",multiplicacion);
+console.log("la division es:",division);
+alert("la suma es: "+ suma);
+
+function sumar(){
+    let numero1 =3;
+    let numero2 =4;
+    let suma=numero1+numero2
+    alert("la suma es: "+ suma);
+
+}
